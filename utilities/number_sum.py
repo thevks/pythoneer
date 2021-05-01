48a2cf2ca43e02f78e4278c128f47ca3585c122c
thevks <vkumar.sharma@gmail.com>
2021-05-01 06:25:14 -0700

    Improving four number matching node

--- utilities/number_sum.py
+++ utilities/number_sum.py
@@ -36,22 +36,38 @@ def threeNumberSum(array, targetSum):
 def fourNumberSum(array, targetSum):
 	array.sort()
 	list = []
-	for i in range(len(array) - 3):
-		left = i + 1
-		right = len(array) - 1
-		while left < right:
-			inner = left + 1
-			while inner < right: 
-				currentSum = array[i] + array[left] + array[inner] + array[right]
-				if currentSum == targetSum:
-					list.append([array[i], array[left], array[inner], array[right]])
-				inner += 1
+	left = 0
+	right = len(array) - 1
+	while left < right :
+		inner_left = left + 1
+		inner_right = right - 1
+		partial_sum = targetSum - (array[left] + array[right])
+		while inner_left < inner_right :
+			current_sum = array[inner_left] + array[inner_right]
+			if current_sum == partial_sum :
+				list.append([array[left], array[inner_left], array[inner_right], array[right]])
+				inner_left += 1
+				inner_right -= 1
+			elif current_sum < partial_sum :
+				inner_left += 1
+			else:
+				inner_right -= 1
+		if (array[left+1] + array[right-1]) < partial_sum:
+			left += 1
+		else:
 			right -= 1
 	return list
 
 if __name__ == '__main__':
-	array = [-8, -6, 1, 2, -3, 4, -5, 6, 8, 3, 5]
-	target = 0
-	print(twoNumberSum(array, target))
-	print(threeNumberSum(array, target))
+	#array = [-8, -6, 1, 2, -3, 4, -5, 6, 8, 3, 5]
+	#target = 0
+	#print(twoNumberSum(array, target))
+	#print(threeNumberSum(array, target))
+	
+	array = [7, 6, 4, -1, 1, 2]
+	target = 16
+	print(fourNumberSum(array, target))
+
+	array = [1, 2, 3, 4, 5, 6, 7]
+	target = 10
 	print(fourNumberSum(array, target))
